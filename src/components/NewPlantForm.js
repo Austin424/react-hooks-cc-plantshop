d05b26f9db7e1dcbdf77b16f003617f0d71bbe8f
Austin Rivera <austin.rivera.24@aol.com>
2024-02-10 06:20:05 -0500

    Plant Shop

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,36 @@
+// import React, { useState } from "react";
+
+// function NewPlantForm(handleSubmit, name, image, price, setName, setImage, setPrice ) {
+
+
+
+
+//   return (
+//     <div className="new-plant-form">
+//       <h2>New Plant</h2>
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" name="name" placeholder="Plant name" value={name} onChange={(event) => setName(event.target.value)} />
+//         <input type="text" name="image" placeholder="Image URL" value={image} onChange={(event) => setImage(event.target.value)} />
+//         <input type="number" name="price" step="0.01" placeholder="Price" value={price} onChange={(event) => setPrice(event.target.name)} />
+//         <button type="submit">Add Plant</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default NewPlantForm;
+
+
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({handleSubmit, name, image, price, setName, setImage, setPrice}) {
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={name} onChange={(event) => setName(event.target.value)}/>
+        <input type="text" name="image" placeholder="Image URL" value={image} onChange={(event) => setImage(event.target.value)}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={price} onChange={(event) => setPrice(event.target.value)}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
-function PlantCard() {
-  return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
-    </li>
-  );
+function PlantCard({name, image, price}) {
+const [stockButton, setStockButton] = useState(true);
+const handleStockButton = () => {
+  setStockButton(stockButton === true ? false : true)
 }
 
+
+
+  return (
+    <>
+        <li className="card" data-testid="plant-item">
+          <img src={image} alt={name} />
+          <h4>{name}</h4>
+          <p>Price: {price}</p>
+          {stockButton ? (
+            <button className="primary" onClick={handleStockButton}>In Stock</button>
+          ) : (
+            <button onClick={handleStockButton}>Out of Stock</button>
+          )}
+        </li>
+    </>
+  );
+}
 export default PlantCard;
